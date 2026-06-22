81cb427672b07b43f13af356fdf5fdf6892648eb
Shawn Renner <shawn.renner@gmail.com>
2026-06-22 15:01:52 -0500

    apply template

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_S3Cmo from "./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Ftesting%2Fcover-image%2Findex.md";
+import __ASTRO_IMAGE_IMPORT_Z38Rwq from "./logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Fmarkdown-elements%2Findex.md";
+export default new Map([["./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Ftesting%2Fcover-image%2Findex.md", __ASTRO_IMAGE_IMPORT_S3Cmo], ["./logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Fmarkdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_Z38Rwq]]);
+		
