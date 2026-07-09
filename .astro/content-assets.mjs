519a551a26266c8512a1ea03f9a7de1c63bf3a60
Shawn Renner <shawn.renner@gmail.com>
2026-07-09 16:07:44 -0500

    cleanup

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_ZulDpU from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
-import __ASTRO_IMAGE_IMPORT_1inyCS from "../../assets/blog-placeholder-4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond-post.md";
 import __ASTRO_IMAGE_IMPORT_Z2vb0o from "../../assets/mcp2515.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fcangrow.md";
-export default new Map([["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZulDpU], ["../../assets/blog-placeholder-4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond-post.md", __ASTRO_IMAGE_IMPORT_1inyCS], ["../../assets/mcp2515.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fcangrow.md", __ASTRO_IMAGE_IMPORT_Z2vb0o]]);
+export default new Map([["../../assets/mcp2515.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fcangrow.md", __ASTRO_IMAGE_IMPORT_Z2vb0o]]);
 		
