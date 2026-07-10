f9d4541266372f0e8e56cff4d7abff3c6ff81a10
Shawn Renner <shawn.renner@gmail.com>
2026-07-10 11:02:36 -0500

    mobile friendly title

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Shawn Renner';
+export const MOBILE_TITLE = 'SR';
 export const SITE_DESCRIPTION = 'Shawn Renner\'s personal homepage';
